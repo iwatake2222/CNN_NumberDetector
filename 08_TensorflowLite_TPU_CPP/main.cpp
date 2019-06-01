b50edbd93dc079e98d599b70a486e646d98d62dc
take-iwiw <take.iwiw2222@gmail.com>
2019-06-01 17:03:03 +0900

    finished 08_TensorflowLite_TPU_CPP

--- 08_TensorflowLite_TPU_CPP/main.cpp
+++ 08_TensorflowLite_TPU_CPP/main.cpp
@@ -1,63 +1,68 @@
 #include <stdio.h>
+#include <string.h>
+#include <chrono>
 #include <opencv2/opencv.hpp>
+
+#include "edgetpu.h"
+#include "utils.h"
 #include "tensorflow/lite/interpreter.h"
-#include "tensorflow/lite/kernels/register.h"
 #include "tensorflow/lite/model.h"
-#include "tensorflow/lite/optional_debug_tools.h"
 
-#define MODEL_FILENAME RESOURCE_DIR"conv_mnist.tflite"
-
-#define TFLITE_MINIMAL_CHECK(x)                              \
-  if (!(x)) {                                                \
-    fprintf(stderr, "Error at %s:%d\n", __FILE__, __LINE__); \
-    exit(1);                                                 \
-  }
+/*** Model parameters ***/
+#define MODEL_FILENAME "mobilenet_v2_1.0_224_quant_edgetpu.tflite"
+// #define MODEL_FILENAME "mobilenet_v2_1.0_224_quant.tflite"
+#define MODEL_WIDTH 224
+#define MODEL_HEIGHT 224
+#define MODEL_CHANNEL 3
 
 
 int main()
 {
-	/* read input image data */
-	cv::Mat image = cv::imread(RESOURCE_DIR"4.jpg");
-	cv::imshow("InputImage", image);
-	
-	/* convert to 28 x 28 grayscale image (normalized: 0 ~ 1.0) */
-	cv::cvtColor(image, image, CV_BGR2GRAY);
-	cv::resize(image, image, cv::Size(28, 28));
-	image = ~image;
-	cv::imshow("InputImage for CNN", image);
-	image.convertTo(image, CV_32FC1, 1.0 / 255);
-
-	/* tflite */
+	/*** Create interpreter ***/
+	/* read model */
 	std::unique_ptr<tflite::FlatBufferModel> model = tflite::FlatBufferModel::BuildFromFile(MODEL_FILENAME);
-	TFLITE_MINIMAL_CHECK(model != nullptr);
+	/* initialize edgetpu_context */
+	edgetpu::EdgeTpuContext* edgetpu_context = edgetpu::EdgeTpuManager::GetSingleton()->NewEdgeTpuContext().release();
+	/* create interpreter */
+	std::unique_ptr<tflite::Interpreter> interpreter = coral::BuildEdgeTpuInterpreter(*model, edgetpu_context);
 
-	// Build the interpreter
-	tflite::ops::builtin::BuiltinOpResolver resolver;
-	tflite::InterpreterBuilder builder(*model, resolver);
-	std::unique_ptr<tflite::Interpreter> interpreter;
-	builder(&interpreter);
-	TFLITE_MINIMAL_CHECK(interpreter != nullptr);
+	/*** Read input image data ***/
+	cv::Mat inputImage = cv::imread("parrot.jpg");
+	cv::cvtColor(inputImage, inputImage, CV_BGR2RGB);
+	cv::resize(inputImage, inputImage, cv::Size(MODEL_WIDTH, MODEL_HEIGHT));
+	std::vector<uint8_t> inputData(inputImage.data, inputImage.data + (inputImage.cols * inputImage.rows * inputImage.elemSize()));
 
-	// Allocate tensor buffers.
-	TFLITE_MINIMAL_CHECK(interpreter->AllocateTensors() == kTfLiteOk);
-	printf("=== Pre-invoke Interpreter State ===\n");
-	tflite::PrintInterpreterState(interpreter.get());
+	/*** Run inference ***/
+	const auto& result = coral::RunInference(inputData, interpreter.get());
 
-	// Set data to input tensor
-	float* input = interpreter->typed_input_tensor<float>(0);
-	memcpy(input, image.reshape(0, 1).data, sizeof(float) * 1 * 28 * 28 * 1);
+	/*** Retrieve result ***/
+	// for (int i = 0; i < result.size(); i++) printf("%5d: %f\n", i, result[i]);
+	auto it_a = std::max_element(result.begin(), result.end());
+	printf("Max index: %ld (%.3f)\n", std::distance(result.begin(), it_a), *it_a);
 
-	// Run inference
-	TFLITE_MINIMAL_CHECK(interpreter->Invoke() == kTfLiteOk);
-	printf("\n\n=== Post-invoke Interpreter State ===\n");
-	tflite::PrintInterpreterState(interpreter.get());
-
-	// Get data from output tensor
-	float* probs = interpreter->typed_output_tensor<float>(0);
-	for (int i = 0; i < 10; i++) {
-		printf("prob of %d: %.3f\n", i, probs[i]);
+	/*** Measure calculation time ***/
+	const auto& t0 = std::chrono::steady_clock::now();
+	for (int i = 0; i < 100; i++) {
+		coral::RunInference(inputData, interpreter.get());
 	}
-
-	cv::waitKey(0);
+	const auto& t1 = std::chrono::steady_clock::now();
+	std::chrono::duration<double> timeSpan = t1 - t0;
+	printf("Calculation time = %f [sec]\n", timeSpan.count() / 100);
+	
 	return 0;
 }
+
+#if 0
+how to build
+mkdir build && cd build
+wget https://dl.google.com/coral/canned_models/mobilenet_v2_1.0_224_quant_edgetpu.tflite
+wget http://download.tensorflow.org/models/tflite_11_05_08/mobilenet_v2_1.0_224_quant.tgz
+tar xfz mobilenet_v2_1.0_224_quant.tgz
+wget https://coral.withgoogle.com/static/docs/images/parrot.jpg
+cp ../external_libs/edgetpu-native/libedgetpu/libedgetpu_arm64.so libedgetpu.so.1
+# cp ../external_libs/edgetpu-native/libedgetpu/libedgetpu_arm32.so libedgetpu.so
+cmake .. -DBUILD_TARGET=JETSON_NATIVE
+# cmake .. -DBUILD_TARGET=RASPI_NATIVE
+make -j2
+sudo LD_LIBRARY_PATH=./ ./NumberDetector
+#endif
